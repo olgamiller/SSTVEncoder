98da439c85ead537bb46c10fb8327b3f50a00040
Olga Miller <olga.rgb@googlemail.com>
2015-01-17 23:00:22 +0100

    WIP: Dragging and Scaling

--- app/src/main/java/om/sstvencoder/CropView.java
+++ app/src/main/java/om/sstvencoder/CropView.java
@@ -19,54 +19,107 @@ package om.sstvencoder;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import om.sstvencoder.Modes.ModeSize;
 
 public class CropView extends ImageView {
-    private ModeSize mNativeSize;
+    private ModeSize mModeSize;
+    private RectF mInputRect;
+    private BitmapRegionDecoder mRegionDecoder;
     private Bitmap mBitmap;
+    private float mPreviousX;
+    private float mPreviousY;
+    private ScaleGestureDetector mScaleDetector;
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float scale = 1.0f / detector.getScaleFactor();
+            float width = mInputRect.width() * scale;
+            float height = mInputRect.height() * scale;
+            if (width < 64.0f || height < 64.0f)
+                return true;
+            if (width > mRegionDecoder.getWidth() || height > mRegionDecoder.getHeight())
+                return true;
+            float dx = (mInputRect.width() - width) / 2.0f;
+            float dy = (mInputRect.height() - height) / 2.0f;
+            mInputRect.inset(dx, dy);
+            return true;
+        }
+    }
 
     public CropView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
     }
 
     public void setModeSize(ModeSize size) {
-        mNativeSize = size;
+        mModeSize = size;
         if (mBitmap != null)
             setImageBitmap(getBitmap());
     }
 
     public void setBitmapStream(InputStream stream) {
-        int bufferBytes = 128 * 1024;
-        if (!stream.markSupported())
-            stream = new BufferedInputStream(stream, bufferBytes);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        stream.mark(bufferBytes);
-        BitmapFactory.decodeStream(new BufferedInputStream(stream), null, options);
-        int scale = Math.max(1, Math.max(options.outWidth / 320, options.outHeight / 256));
-        options.inSampleSize = Integer.highestOneBit(scale);
-        options.inJustDecodeBounds = false;
         try {
-            stream.reset();
-            mBitmap = BitmapFactory.decodeStream(stream, null, options);
-            setImageBitmap(getBitmap());
+            mRegionDecoder = BitmapRegionDecoder.newInstance(stream, true);
+            mInputRect = new RectF(0.0f, 0.0f, mRegionDecoder.getWidth(), mRegionDecoder.getHeight());
+            update();
         } catch (IOException ignore) {
         }
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        mScaleDetector.onTouchEvent(e);
+        float x = e.getX();
+        float y = e.getY();
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dx = (mInputRect.width() * (mPreviousX - x)) / getWidth();
+                float dy = (mInputRect.height() * (mPreviousY - y)) / getHeight();
+                dx = Math.max(0.0f, mInputRect.left + dx) - mInputRect.left;
+                dy = Math.max(0.0f, mInputRect.top + dy) - mInputRect.top;
+                dx = Math.min(mRegionDecoder.getWidth(), mInputRect.right + dx) - mInputRect.right;
+                dy = Math.min(mRegionDecoder.getHeight(), mInputRect.bottom + dy) - mInputRect.bottom;
+                mInputRect.offset(dx, dy);
+                update();
+        }
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
+    }
+
+    public void update() {
+        Rect rect = new Rect((int) mInputRect.left, (int) mInputRect.top, (int) mInputRect.right, (int) mInputRect.bottom);
+        mBitmap = mRegionDecoder.decodeRegion(rect, getBitmapOptions());
+        setImageBitmap(mBitmap);
+    }
+
+    private BitmapFactory.Options getBitmapOptions() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        int sx = Math.round(mInputRect.width() / mModeSize.getWidth());
+        int sy = Math.round(mInputRect.height() / mModeSize.getHeight());
+        int scale = Math.max(1, Math.max(sx, sy));
+        options.inSampleSize = Integer.highestOneBit(scale);
+        return options;
+    }
+
     public Bitmap getBitmap() {
-        return scaleBitmap(mBitmap, mNativeSize.getWidth(), mNativeSize.getHeight());
+        return scaleBitmap(mBitmap, mModeSize.getWidth(), mModeSize.getHeight());
     }
 
     private Bitmap scaleBitmap(Bitmap bmp, int ow, int oh) {
